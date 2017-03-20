a2e4344d97731f9ff9d1c734859e736cf211d3eb
Melon Bao <melon@raspberry.pi>
2017-03-21 00:50:42 +0800

    for group page shows(need to adjust css style later)

---
+++ parser.php
@@ -0,0 +1,10 @@
+<?php
+// Parse without sections
+//$ini_array = parse_ini_file("queues.conf");
+//print_r($ini_array);
+
+// Parse with sections
+$ini_array = parse_ini_file("queues.conf", true);
+//print_r($ini_array);
+
+?>
