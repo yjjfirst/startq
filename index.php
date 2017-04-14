dda84658b25b12c2f6a39520ed9424a8f4065288
Melon Bao <melon.bao@raspberry.pi>
2017-04-14 15:24:06 +0800

    For page resizing to adjust the font size

--- index.php
+++ index.php
@@ -1,110 +1,16 @@
 <html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<!--<meta name="viewport" content="width=640" />-->
+<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<meta name="apple-mobile-web-app-title" content="">
+<meta name="format-detection" content="telephone=no">
+<title> </title>
+<link rel="stylesheet" href="./style.css">
+
 <!-- CSS goes in the document HEAD or added to your external stylesheet -->
-<style type="text/css">
-table.imagetable {
-    font-family: verdana,arial,sans-serif;
-    width:100%;
-	font-size:11px;
-	color:#333333;
-	border-width: 1px;
-	border-color: #999999;
-	border-collapse: collapse;
-}
-table.imagetable th {
-	background:#b5cfd2 url('./images/cell-blue.jpg');
-	border-width: 1px;
-	padding: 8px;
-	border-style: solid;
-	border-color: #999999;
-}
-
-table.imagetable th.top {
-	background:#CCCCCC;
-	border-width: 1px;
-	padding: 8px;
-	border-style: solid;
-	border-color: #999999;
-}
-
-table.imagetable td.odd {
-	background:#FFFFFF;
-	border-width: 1px;
-	padding: 2px;
-	border-style: solid;
-	border-color: #999999;
-}
-
-table.imagetable td.even {
-	background:#F9F9F9;
-	border-width: 1px;
-	padding: 2px;
-	border-style: solid;
-	border-color: #999999;
-}
-
-table.imagetable td.blank {
-	background:#FFFFFF;
-	border-width: 1px;
-	padding: 10px;
-	border-style: solid;
-	border-color: #FFFFFF;
-}
-
-table.imagetable td.yellow{
-	background:#FFCC00;
-	border-width: 1px;
-	padding: 2px;
-	border-style: solid;
-	border-color: #999999;
-}
-	
-table.imagetable td.red{
-	background:#C00000;
-	border-width: 1px;
-	padding: 2px;
-	border-style: solid;
-	border-color: #999999;
-}
-
-table.imagetable td.green{
-	background:#92D050;
-	border-width: 1px;
-	padding: 2px;
-	border-style: solid;
-	border-color: #999999;
-}
-table.imagetable td.yellowish{
-	background:#BF8F00;
-	border-width: 1px;
-	padding: 2px;
-	border-style: solid;
-	border-color: #999999;
-}
-table.imagetable td.yellow{
-    background:#FFFF00;
-    border-width: 1px;
-    padding: 2px;
-    border-style: solid;
-    border-color: #999999;
-}
-
-table.imagetable td.black{
-	background:#040302;
-	color:#FFFFCC;
-	border-width: 1px;
-	padding: 2px;
-	border-style: solid;
-	border-color: #999999;
-}
-table.imagetable td.pink{
-	background:#FF6666;
-	border-width: 1px;
-	padding: 2px;
-	border-style: solid;
-	border-color: #999999;
-}
-
-</style>
 <script language="JavaScript"> 
 function page_reload() 
 { 
@@ -133,10 +39,11 @@ function page_reload()
 setTimeout('page_reload()',3000); 
 </script> 
 <body>
-
+<div class="words">
 <?php
 include_once("./group_table.php");
 include_once("./agent_table.php");
 ?>
+</div>
 </body>
 </html>
